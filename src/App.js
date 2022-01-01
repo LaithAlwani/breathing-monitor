08b0297ee92d1d28cda0225d1f8bd48808ed904b
Laith Alwani <laithalwani@gmail.com>
2021-12-31 23:30:43 -0500

    added breathing images

--- src/App.js
+++ src/App.js
@@ -3,49 +3,46 @@ import "./App.css";
 
 function App() {
   const [breathCount, setBreathCount] = useState(0);
-  const [ breathTimer, setBreathTimer] = useState(1)
+  const [breathTimer, setBreathTimer] = useState(1);
   const [message, setMessage] = useState("");
   const [isActivated, setIsActivated] = useState(false);
 
   const breathing = () => {
     let timerIn = 1;
     let timerOut = 1;
-    let breathCount = 0
-    setMessage("")
+    let breathCount = 0;
+    setMessage("");
     setMessage("Breath IN");
-    setIsActivated(true)
+    setIsActivated(true);
     const breathInt = setInterval(() => {
       if (breathCount < 30) {
-       
         if (timerIn < 2) {
-          timerIn++
-          setBreathTimer(timerIn)
+          timerIn++;
+          setBreathTimer(timerIn);
         } else {
           if (timerOut <= 4) {
             setMessage("Breath OUT");
-            setBreathTimer(timerOut)
-            timerOut++
+            setBreathTimer(timerOut);
+            timerOut++;
           } else {
             setMessage("Breath IN");
-            timerIn = 1
-            setBreathTimer(timerIn)
-            timerOut=1
-            breathCount++
-            setBreathCount(breathCount)
+            timerIn = 1;
+            setBreathTimer(timerIn);
+            timerOut = 1;
+            breathCount++;
+            setBreathCount(breathCount);
           }
-          
         }
       } else {
-        clearInterval(breathInt)
-        setMessage("DONE")
-        setIsActivated(false)
-        breathCount = 0
-        setBreathCount(breathCount)
+        clearInterval(breathInt);
+        setMessage("DONE");
+        setIsActivated(false);
+        breathCount = 0;
+        setBreathCount(breathCount);
         setTimeout(() => {
           setMessage("");
-        },2000)
-     }
-      
+        }, 2000);
+      }
     }, 1000);
   };
 
@@ -56,14 +53,31 @@ function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <h2>Breath Count: {breathCount}</h2>
-        {message && <div className={message === "Breath OUT" ? "message red" :"message"}>{message}</div>}
+        <h1>Breath Count: {breathCount}</h1>
+        {message && (
+          <div
+            className={message === "Breath OUT" ? "message red" : "message"}
+          >
+            {message}{" "}
+            <img
+              src={
+                message === "Breath OUT"
+                  ? "images/breath-out.png"
+                  : "images/breath-in.png"
+              }
+              alt="breathing"
+              className="image"
+            />{" "}
+          </div>
+        )}
         {!isActivated ? (
-          <button className="btn" onClick={handleClick}>Start</button>
+          <button className="btn" onClick={handleClick}>
+            Start
+          </button>
         ) : (
           <>
-            <p>{breathTimer}</p>
-            {breathCount >= 30 &&  <p>workout is Over</p>}
+            <h1>{breathTimer}</h1>
+            {breathCount >= 30 && <p>workout is Over</p>}
           </>
         )}
       </div>
